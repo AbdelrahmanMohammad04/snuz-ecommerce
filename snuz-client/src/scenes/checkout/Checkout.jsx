411ff61fc92bc36c3e87be98ae319915f3f4a9c6
abood mohammad <abdelrahmanmohammad89@gmail.com>
2023-07-05 00:27:12 -0500

    Added Shipping form, Address form, and Payment form. Bug fixes

--- snuz-client/src/scenes/checkout/Checkout.jsx
+++ snuz-client/src/scenes/checkout/Checkout.jsx
@@ -4,6 +4,7 @@ import { Formik } from "formik"
 import { useState } from "react"
 import * as yup from "yup"
 import Shipping from "./Shipping"
+import Payment from "./Payment"
 import { shades } from "../../theme"
 
 const initialValues = {
@@ -84,13 +85,27 @@ const checkoutSchema = [
 ]
 
 const Checkout = () => {
-    const [activeStep, setActiveSet] = useState(0)
+    const [activeStep, setActiveStep] = useState(0)
     const cart = useSelector((state) => state.cart.cart)
     const isFirstStep = activeStep === 0
     const isSecondStep = activeStep === 1
 
-    const handleFormSubmit = async(value, action) => {
-        setActiveSet(activeStep + 1)
+    const handleFormSubmit = async(values, actions) => {
+        setActiveStep(activeStep + 1)
+
+        //copy billing address onto shipping
+        if (isFirstStep && values.shippingAddress.isSameAddress) {
+            actions.setFieldValue("shippingAddress", {
+                ...values.billingAddress,
+                isSameAddress: true
+            })
+        }
+
+        if (isSecondStep) {
+            makePayment(values)
+        }
+
+        actions.setTouched({})
     }
 
     async function makePayment(values) {
